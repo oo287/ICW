a7baf0ba25aab1046c30fabf8533fefd22cec941
Oliver O'Toole <oo287@cam.ac.uk>
2022-02-07 13:20:16 +0000

    PUSH

--- equation_solver.py
+++ equation_solver.py
@@ -1,21 +1,22 @@
 import numpy as np
 
-def y_amplitudes(m_mat, k_mat, omega, f_vector):
+def y_amplitudes(m_mat, k_mat, l_mat, omega, f_vector):
 
     m_mat = np.array(m_mat)
     k_mat = np.array(k_mat)
+    l_mat = np.array(l_mat)
     f_vector = np.array(f_vector)
 
     y_vector = np.zeros(len(f_vector))
     
     # y_vector = [k_mat - omega^2 * m_mat]^-1 * f_vector
 
-    dyn_mat = k_mat - omega*omega*m_mat
+    dyn_mat = k_mat + omega*l_mat*1j - omega*omega*m_mat
 
     inv_dyn_mat = np.linalg.inv(dyn_mat)
 
-    y_vector = inv_dyn_mat @ f_vector
+    y_vector = np.array(inv_dyn_mat @ f_vector)
     
-    return y_vector
+    return abs(y_vector)
 
-#y_amplitudes([[100, 0],[0, 1]],[[-1, 1],[1, -1]],1,[0,1])
+#print(y_amplitudes([[100, 0],[0, 1]],[[-1, 1],[1, -1]],[[-1, 1],[1, -1]],1,[0,1]))
