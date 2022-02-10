004c8e42e786dd8c5f75778a9d72ce0e019c9a95
Finn Sutcliffe <finn.sutcliffe@gmail.com>
2022-02-10 09:25:22 +0000

    Fixed 2dof_ntmd configuration, fixed colours on comparer
    Signed-off-by: Finn Sutcliffe <finn.sutcliffe@gmail.com>

--- response_comparer.py
+++ response_comparer.py
@@ -1,18 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#"""
+names = [
+        "data_exp1_2dof_0tmd.txt.txt",
+        "data_exp1_2dof_1tmd.txt.txt",
+        "data_exp1_2dof_ntmd.txt.txt",
+        "data_exp1_2dof_2tmdd.txt.txt",
+        "data_exp1_2dof_2tmds.txt.txt"
+        ]
+dofs = 2
+#"""
+
+"""
 names = [
         "data_exp2_3dof_0tmd.txt.txt",
+        "data_exp2_3dof_1tmd.txt.txt",
         "data_exp2_3dof_ntmd.txt.txt",
         "data_exp2_3dof_3tmdd.txt.txt",
         "data_exp2_3dof_3tmds.txt.txt"
         ]
+dofs = 3
+"""
+
+"""
+names = [
+        "data_exp3_4dof_0tmd.txt.txt",
+        "data_exp3_4dof_1tmd.txt.txt",
+        "data_exp3_4dof_ntmd.txt.txt",
+        "data_exp3_4dof_4tmdd.txt.txt",
+        "data_exp3_4dof_4tmds.txt.txt"
+        ]
+dofs = 4
+"""
+
+colours = ["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple"]
 
 n = 100000
 
 omegas = np.linspace(0.01,100,n)
 
-dofs = 3
 
 amplitudes = np.zeros((len(names),n,dofs))
 
@@ -41,7 +68,7 @@ for j in range(0,len(names)):
 
     for i in range(0,dofs):
         
-        plt.plot(omegas,amplitudes[j,:,i],label=str(names[j]))
+        plt.plot(omegas,amplitudes[j,:,i],label=str(names[j]), color=colours[j])
 
 plt.xlabel("Excitation Frequency / rad s-1")
 plt.ylabel("Amplitude / m")
